7bb056bdbb384dca850be4d671020f5710465d5a
Mariam Elfeky <mariamelfeky123@gmail.com>
2020-07-18 16:08:17 +0200

    migration

--- LZ-for-animals/database/migrations/2014_10_12_000000_create_users_table.php
+++ LZ-for-animals/database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,15 +11,24 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
+    
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('user_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->enum('role', ['doctor', 'clinic','admin']);
+            $table->string('card');
+            $table->string('image');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('location');
+            $table->boolean('is_verified')->default(0);
             $table->timestamps();
         });
     }
